f55baa3f0f601e97814d2ccf5153d9fcc7b1db66
sunecosuri <sunecosuri@pepabo.com>
2018-01-09 12:39:24 +0900

    add blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,27 +1,66 @@
 <template>
-<section class="content-container">
-</section>
+  <section class="home-container">
+    <div>
+      <h2 class="home-slogan">
+        記事一覧ページ
+      </h2>
+      <div class="home-content">
+        <p v-for="post in posts">
+          <a :href="post.permalink">{{post.title}}</a>
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
+  asyncData: async ({ app }) => ({
+    posts: await app.$content('/').getAll()
+  })
 }
 </script>
 
-<style scoped>
-.content-container
+<style>
+.home-container
 {
-  width: 38rem;
-  margin: 0 auto;
-  margin-top: 2frem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
-
-.post-title
+.home-content
 {
-  font-size: 2.5rem;
-  margin-top: 1rem;
-  margin-bottom: 1rem;
-  font-weight: 500;
-  color: #35495e;
+  margin: 0 auto;
+  margin-top: 5px;
+  padding: 30px;
+}
+.home-slogan
+{
+  font-weight: 300;
+  font-size: 42px;
+  color: #fff;
+  word-spacing: 5px;
+  text-align: center;
+  padding-top: 80px;
+  padding-bottom: 15px;
+}
+.home-links
+{
+  padding-top: 15px;
+  background-color: #fff;
+}
+.home-content .p 
+{
+  text-align: left;
+  margin: 0 auto;
+  color: #333;
+  display: block;
+  -webkit-margin-before: 1em;
+  -webkit-margin-after: 1em;
+  -webkit-margin-start: 0px;
+  -webkit-margin-end: 0px;
+  margin: 0 auto;
 }
 </style>
+
